d77bbd0f49063240f1f6e19c202e03e89e24ed0b
Yahel Shechter <yahelshe@gmail.com>
2026-04-12 13:43:05 +0300

    icon #5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico?v=1" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
       </head>
       <body style={bodyStyle}>
         <div style={appShell}>
